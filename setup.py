151c68397dcb4897c16539b099933ca7251e66ec
Roger Camargo <huogerac@gmail.com>
2021-06-21 14:36:36 -0300

    Make the dockerpy version more flexible

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="jarlesssdk",
-    version="0.0.2",
+    version="0.0.3",
     py_modules=[
         "jarless_sdk",
     ],
-    install_requires=["Click", "docker==4.4.4", "PyYAML==5.4.1"],
+    install_requires=["Click", "docker>=4.3.0", "PyYAML>=5.0.0"],
     entry_points="""
         [console_scripts]
         jarless=jarless_sdk.sdk:cli
